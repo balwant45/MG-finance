15ab32b05ebc9815afbd14b027b9f910fd42665e
Balwant Singh <bawabalwant045@gmail.com>
2025-10-29 15:04:24 +0530

    add CORS in server.js file

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,16 @@ import loanRoutes from "./routes/Loans.route.js";
 // import installmentRoutes from "./routes/Installment.route.js"; // if you created this
 
 dotenv.config();
+// Configure CORS
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow only your frontend's origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific HTTP methods
+  credentials: true, // Allow cookies to be sent
+  optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions)); // Use CORS middleware with your options
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
