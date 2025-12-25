ff27a2510411b0688f771966bde184d75550e9be
Balwant Singh <bawabalwant045@gmail.com>
2025-12-25 22:58:21 +0530

    set cors with "https://www.mgfinances.com/"

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // ✅ CORS: Correct setup for Cookies
 app.use(cors({
-  origin: "https://www.mgfinances.com", // MUST match your React frontend URL exactly
+  origin:"https://www.mgfinances.com/",// origin: "http://localhost:5173", // MUST match your React frontend URL exactly
   credentials: true                // Required for cookies to work
 }));
 
