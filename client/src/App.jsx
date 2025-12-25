c611e34eff4a5eafa94c16ecda959dc00d76ef7b
Balwant Singh <bawabalwant045@gmail.com>
2025-12-25 23:17:50 +0530

    revisit axios setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import CreateCustomer from "./pages/DashboardPages/CreateCustomer";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 // ✅ 1. AXIOS SETUP
-axios.defaults.baseURL = 'https://mg-finance-7.onrender.com'; 
+axios.defaults.baseURL = 'https://mg-finance.onrender.com'; 
 // axios.defaults.baseURL = 'http://localhost:3000'; 
 axios.defaults.withCredentials = true; // 🚨 CRITICAL: Allows cookies to be sent/received
 
